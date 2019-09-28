51bf39d3b0387b0843a8ce5bd5ef29e70b9e372e
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-09-28 20:57:33 +0900

    wip(agc038)

--- contests/agc038/tasks/agc038_a/main.go
+++ contests/agc038/tasks/agc038_a/main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"log"
-	"math"
 	"strconv"
 	"strings"
 )
 
-func Matrix(h, w, a, b int) ([][]int, error) {
+func Matrix(h, w, a, b int) [][]int {
 	log.Printf("h: %d", h)
 	log.Printf("w: %d", w)
 	log.Printf("a: %d", a)
@@ -20,45 +18,35 @@ func Matrix(h, w, a, b int) ([][]int, error) {
 		dary[i] = make([]int, w)
 	}
 
-	// 初期値で条件を満たすか確認
-	acount := make(map[int]int)
-	for i := 0; i < h; i++ {
-		for j := 0; j < w; j++ {
-			acount[dary[i][j]]++
-			if math.Min(float64(acount[0]), float64(acount[1])) == float64(a) {
-				return dary, nil
+	for i := 0; i <= h; i++ {
+		for j := 0; j <= w; j++ {
+			if i+1 == b && j+1 == a {
+				dary[i][j] = 0
+			}
+			if i+1 == b && j+1 == w-a {
+				dary[i][j] = 1
+			}
+			if i+1 == h-b && j+1 == a {
+				dary[i][j] = 1
+			}
+			if i+1 == h-b && j+1 == w-a {
+				dary[i][j] = 0
 			}
 		}
 	}
-
-	// 初期値で条件を満たさない場合、bluteforceで逐一条件を満たすか確認する
-	for i := 0; i < h; i++ {
-		for j := 0; j < w; j++ {
-			dary[i][j] = 1
-			log.Print(dary)
-			acount[dary[i][j]]++
-			if math.Min(float64(acount[0]), float64(acount[1])) == float64(a) {
-				return dary, nil
-			}
-		}
-	}
-	return nil, errors.New("NG")
+	return dary
 }
 
 func main() {
 	var h, w, a, b int
 	fmt.Scan(&h, &w, &a, &b)
-	matrix, err := Matrix(h, w, a, b)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(matrix)
-		for _, l := range matrix {
-			sl := make([]string, 0, w)
-			for _, s := range l {
-				sl = append(sl, strconv.Itoa(s))
-			}
-			fmt.Println(strings.Join(sl, ""))
+	matrix := Matrix(h, w, a, b)
+	fmt.Println(matrix)
+	for _, l := range matrix {
+		sl := make([]string, 0, w)
+		for _, s := range l {
+			sl = append(sl, strconv.Itoa(s))
 		}
+		fmt.Println(strings.Join(sl, ""))
 	}
 }
