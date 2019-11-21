cbda4a1c235d0f888d074a89728b80f1eca9e1d3
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-11-21 15:52:37 +0900

    feat(01 Matrix): refarence version
    ## refarence
    - https://atcoder.jp/contests/agc038/submissions/7672774

--- contests/agc038/tasks/agc038_a/main.go
+++ contests/agc038/tasks/agc038_a/main.go
@@ -2,34 +2,29 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"strconv"
 	"strings"
 )
 
 func Matrix(h, w, a, b int) [][]int {
-	log.Printf("h: %d", h)
-	log.Printf("w: %d", w)
-	log.Printf("a: %d", a)
-	log.Printf("b: %d", b)
 	// Slice of Slice
 	dary := make([][]int, h)
 	for i := range dary {
 		dary[i] = make([]int, w)
 	}
 
-	for i := 0; i <= h; i++ {
-		for j := 0; j <= w; j++ {
-			if i+1 == b && j+1 == a {
+	for i := 0; i < h; i++ {
+		for j := 0; j < a; j++ {
+			if i < b {
 				dary[i][j] = 0
-			}
-			if i+1 == b && j+1 == w-a {
+			} else {
 				dary[i][j] = 1
 			}
-			if i+1 == h-b && j+1 == a {
+		}
+		for j := a; j < w; j++ {
+			if i < b {
 				dary[i][j] = 1
-			}
-			if i+1 == h-b && j+1 == w-a {
+			} else {
 				dary[i][j] = 0
 			}
 		}
@@ -41,9 +36,8 @@ func main() {
 	var h, w, a, b int
 	fmt.Scan(&h, &w, &a, &b)
 	matrix := Matrix(h, w, a, b)
-	fmt.Println(matrix)
 	for _, l := range matrix {
-		sl := make([]string, 0, w)
+		sl := make([]string, w)
 		for _, s := range l {
 			sl = append(sl, strconv.Itoa(s))
 		}
