dc5275c7c61824497c8e6c813ec111250222d30f
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-12-09 21:58:00 +0900

    draft(abc147/D - Xor Sum 4): tle & wa -> tle

---
+++ contests/abc147/tasks/abc147_d/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const MOD = 1000000007
+
+func xorSum4(n int, a []int) int {
+
+	var sum int
+	for i := 0; i < n-1; i++ {
+		for j := i + 1; j < n; j++ {
+			// Devide because it overflows.
+			// SeeAlso: https://qiita.com/drken/items/3b4fdf0a78e7a138cd9a#オーバーフローに注意
+			sum += (a[i] ^ a[j]) % MOD
+		}
+	}
+	return sum % MOD
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 10240*10240)
+
+	n, err := strconv.Atoi(readLine(reader))
+	checkError(err)
+	a := make([]int, n)
+	ss := strings.Split(readLine(reader), " ")
+	for i := range ss {
+		a[i], err = strconv.Atoi(ss[i])
+		checkError(err)
+	}
+	fmt.Println(xorSum4(n, a))
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
