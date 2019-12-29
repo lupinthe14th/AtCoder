32a3b6000f650fbc597978a6c31d4a25f61ff8e8
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-12-29 20:49:29 +0900

    fix(abc149/B - Greedy Takahashi): overflow

--- contests/abc149/tasks/abc149_b/main.go
+++ contests/abc149/tasks/abc149_b/main.go
@@ -4,11 +4,11 @@ import (
 	"fmt"
 )
 
-func solution(a, b, k uint64) []uint64 {
-	ans := make([]uint64, 0, 2)
+func solution(a, b, k int64) []int64 {
+	ans := make([]int64, 0, 2)
 	if a >= k {
 		a -= k
-		ans = []uint64{a, b}
+		ans = []int64{a, b}
 	} else if a < k {
 		k -= a
 		a = 0
@@ -16,13 +16,13 @@ func solution(a, b, k uint64) []uint64 {
 		if b < 0 {
 			b = 0
 		}
-		ans = []uint64{a, b}
+		ans = []int64{a, b}
 	}
 	return ans
 }
 
 func main() {
-	var a, b, k uint64
+	var a, b, k int64
 	fmt.Scan(&a, &b, &k)
 	ans := solution(a, b, k)
 	fmt.Printf("%d %d", ans[0], ans[1])
--- contests/abc149/tasks/abc149_b/main_test.go
+++ contests/abc149/tasks/abc149_b/main_test.go
@@ -7,16 +7,20 @@ import (
 )
 
 type input struct {
-	a, b, k uint64
+	a, b, k int64
 }
 
 var cases = []struct {
 	id    int
 	input input
-	want  []uint64
+	want  []int64
 }{
-	{id: 1, input: input{a: 2, b: 3, k: 3}, want: []uint64{0, 2}},
-	{id: 2, input: input{a: 500000000000, b: 500000000000, k: 1000000000000}, want: []uint64{0, 0}},
+	{id: 1, input: input{a: 2, b: 3, k: 3}, want: []int64{0, 2}},
+	{id: 2, input: input{a: 500000000000, b: 500000000000, k: 1000000000000}, want: []int64{0, 0}},
+	{id: 3, input: input{a: 0, b: 0, k: 0}, want: []int64{0, 0}},
+	{id: 4, input: input{a: 2, b: 3, k: 2}, want: []int64{0, 3}},
+	{id: 5, input: input{a: 5, b: 3, k: 3}, want: []int64{2, 3}},
+	{id: 6, input: input{a: 5, b: 3, k: 9}, want: []int64{0, 0}},
 }
 
 func TestSolution(t *testing.T) {
