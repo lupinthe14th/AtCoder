3c4d316f6b7f11b3a318aefd8aa6efad65f15fd7
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-12-29 20:07:35 +0900

    feat(abc149/A - Strings): Using fmt.Sprint

---
+++ contests/abc149/tasks/abc149_a/main_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+type input struct {
+	s, t string
+}
+
+var cases = []struct {
+	id    int
+	input input
+	want  string
+}{
+	{id: 1, input: input{s: "oder", t: "atc"}, want: "atcoder"},
+	{id: 2, input: input{s: "humu", t: "humu"}, want: "humuhumu"},
+}
+
+func TestSolution(t *testing.T) {
+	for _, tt := range cases {
+		t.Run(fmt.Sprint(tt.id), func(t *testing.T) {
+			got := solution(tt.input.s, tt.input.t)
+			if got != tt.want {
+				t.Errorf("%s, want: %s", got, tt.want)
+			}
+		})
+	}
+}
