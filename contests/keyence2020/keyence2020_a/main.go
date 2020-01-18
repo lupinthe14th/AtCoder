17ec206cfe04b0991b28119a7b52ccc1500c7d14
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-01-18 21:15:48 +0900

    feat(keyence2020/A - Painting): wa

---
+++ contests/keyence2020/keyence2020_a/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func solution(h, w, n int) int {
+	max := max(h, w)
+	var ans int = 1
+	for max*ans < n {
+		ans++
+	}
+	return ans
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func main() {
+	var h, w, n int
+	fmt.Scan(&h, &w, n)
+	fmt.Println(solution(h, w, n))
+}
