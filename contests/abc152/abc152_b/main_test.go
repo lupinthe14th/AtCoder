584949387dcb1db1e7861d69bfcd9c8f212da215
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-01-19 21:30:15 +0900

    feat(abc152/B - Comparing Strings): I could self solve
    WA*3

---
+++ contests/abc152/abc152_b/main_test.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+	"testing"
+)
+
+type input struct {
+	a, b int
+}
+
+type Case struct {
+	input input
+	want  int
+}
+
+var cases = []Case{
+	{input: input{a: 4, b: 3}, want: 3333},
+	{input: input{a: 7, b: 7}, want: 7777777},
+}
+
+func TestSolution(t *testing.T) {
+	for i, tt := range cases {
+		t.Run(fmt.Sprint(i), func(t *testing.T) {
+			got := solution(tt.input.a, tt.input.b)
+			if got != tt.want {
+				t.Errorf("%d, want: %d", got, tt.want)
+			}
+		})
+	}
+}
+
+func Example_main() {
+	s := strings.NewReader("4 3")
+	b, _ := ioutil.ReadAll(s)
+	inr, inw, _ := os.Pipe()
+	orgStdin := os.Stdin
+	inw.Write(b)
+	inw.Close()
+	os.Stdin = orgStdin
+	os.Stdin = inr
+
+	main()
+
+	os.Stdin = orgStdin
+
+	// Output: 3333
+}
