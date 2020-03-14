e28ea8173512e2fd697813bd929f8738cb927aa8
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-03-14 22:22:07 +0900

    wip(panasonic2020/C - Sqrt Inequality): ???

---
+++ contests/panasonic2020/panasonic2020_c/main_test.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+type in struct {
+	a, b, c float64
+}
+
+type Case struct {
+	in   in
+	want string
+}
+
+var cases = []Case{
+	{in: in{a: 2, b: 3, c: 9}, want: "No"},
+}
+
+func TestSolution(t *testing.T) {
+	for i, tt := range cases {
+		t.Run(fmt.Sprint(i), func(t *testing.T) {
+			got := solution(tt.in.a, tt.in.b, tt.in.c)
+			if got != tt.want {
+				t.Errorf("in: %+v, got: %s, want: %s", tt.in, got, tt.want)
+			}
+		})
+	}
+}
+
+func Example_main() {
+	c, _ := ioutil.ReadFile("./input.txt")
+	inr, inw, _ := os.Pipe()
+	orgStdin := os.Stdin
+	inw.Write(c)
+	inw.Close()
+	os.Stdin = orgStdin
+	defer func() { os.Stdin = orgStdin }()
+	os.Stdin = inr
+
+	main()
+
+	// Output: Yes
+}
