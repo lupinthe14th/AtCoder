d0dc0cf761c29671186b6ade9116207feab74239
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-04-04 22:47:29 +0900

    B - Popular Vote: arithmetic

---
+++ contests/abc161/abc161_b/main_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestSolution(t *testing.T) {
+	type in struct {
+		n, m int
+		a    []int
+	}
+
+	var cases = []struct {
+		in   in
+		want string
+	}{
+		{in: in{n: 4, m: 1, a: []int{5, 4, 2, 1}}, want: "Yes"},
+		{in: in{n: 3, m: 2, a: []int{380, 19, 1}}, want: "No"},
+	}
+	for i, tt := range cases {
+		t.Run(fmt.Sprint(i), func(t *testing.T) {
+			got := solution(tt.in.n, tt.in.m, tt.in.a)
+			if got != tt.want {
+				t.Errorf("in: %+v, got: %s, want: %s", tt.in, got, tt.want)
+			}
+		})
+	}
+}
+
+func Example_main() {
+	c, _ := ioutil.ReadFile("./input.txt")
+	inr, inw, _ := os.Pipe()
+	orgStdin := os.Stdin
+	inw.Write(c)
+	inw.Close()
+	os.Stdin = orgStdin
+	defer func() { os.Stdin = orgStdin }()
+	os.Stdin = inr
+
+	main()
+
+	// Output: Yes
+}
