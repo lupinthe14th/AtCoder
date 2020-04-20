7020a930f4e724e18d2b064ac8f8c5bceab27ac0
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-04-20 11:12:31 +0900

    abc163: refactor code and add comment

--- contests/abc163/abc163_d/main.go
+++ contests/abc163/abc163_d/main.go
@@ -7,13 +7,17 @@ import (
 // SeeAlso
 // - https://atcoder.jp/contests/abc163/submissions/12126659
 // - https://atcoder.jp/contests/abc163/submissions/12132835
+// - https://youtu.be/MEtKSRdM4YI?t=428
 func solution(n, k int) int {
 	ans := 0
 	for i := k; i <= n+1; i++ {
+		// min := ((i - 1) * i) / 2
+		// max := min + i*(n-i+1)
+		// ans += max - min + 1
 		ans += i*(n-i+1) + 1
-		ans %= (1e9 + 7)
+		ans %= 1e9 + 7
 	}
-	return ans % (1e9 + 7)
+	return ans
 }
 
 func main() {
