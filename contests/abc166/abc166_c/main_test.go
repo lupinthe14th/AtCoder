7a2e24fcfd4552aaa2fcedf317f4180d465d630b
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-05-06 22:30:16 +0900

    abc166/C - Peaks: using max value

---
+++ contests/abc166/abc166_c/main_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestSolution(t *testing.T) {
+	type in struct {
+		h      []int
+		matrix [][]int
+	}
+	var cases = []struct {
+		in   in
+		want int
+	}{
+		{in: in{h: []int{1, 2, 3, 4}, matrix: [][]int{{1, 3}, {2, 3}, {2, 4}}}, want: 2},
+	}
+	for i, tt := range cases {
+		t.Run(fmt.Sprint(i), func(t *testing.T) {
+			got := solution(tt.in.h, tt.in.matrix)
+			if got != tt.want {
+				t.Errorf("in: %+v, got: %v, want: %v", tt.in, got, tt.want)
+			}
+		})
+	}
+}
+
+func Example_main() {
+	c, _ := ioutil.ReadFile("./input.txt")
+	inr, inw, _ := os.Pipe()
+	orgStdin := os.Stdin
+	inw.Write(c)
+	os.Stdin = orgStdin
+	defer func() {
+		inw.Close()
+		os.Stdin = orgStdin
+	}()
+	os.Stdin = inr
+
+	main()
+
+	// Output: 3
+}
