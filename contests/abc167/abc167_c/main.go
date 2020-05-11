78f3374ea4fbfbc960ead54d09d01b615e418f71
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-05-11 11:29:20 +0900

    abc167/C - Skill Up: ac solution: bit 全探索

--- contests/abc167/abc167_c/main.go
+++ contests/abc167/abc167_c/main.go
@@ -2,54 +2,57 @@ package main
 
 import (
 	"fmt"
-	"log"
 )
 
+// bit 全探索
+// 全探索にはDFSもあるが、部分集合を全探索する場合に有用なbit全探索を用いた
+// 買う: 1
+// 買わない: 0
+//
+// 0,1,2
+// -----
+// 0,0,0
+// 0,0,1
+// 0,1,0
+// 0,1,1
+// 1,0,0
+// 1,0,1
+// 1,1,0
+// 1,1,1
+// https://youtu.be/ENSOy8u9K9I?t=1302
 func solution(n, m, x int, a [][]int, c []int) int {
-	s := make([][]int, n+1)
-	for i := range s {
-		s[i] = make([]int, m)
-	}
-	for i := 1; i <= n; i++ {
-		for j := 0; j < m; j++ {
-			s[i][j] = s[i-1][j] + a[i-1][j]
+	ans := 1 << 31
+	// s: 部分集合
+	// 1<<uint(n)
+	// n = 3の場合, 1000 -> 8
+	// 1<<n -> 2^n
+	for s := 0; s < 1<<uint(n); s++ {
+		cost := 0
+		d := make([]int, m)
+		for i := 0; i < n; i++ {
+			// iビット目が1か調べる
+			// 1. 右ビットシフト1してiビット目を一番右に移動する（最下位ビットにシフトする）
+			// 2. 最下位ビットを取り出すため &1する（両方が1なら1）
+			if s>>uint(i)&1 == 1 {
+				cost += c[i]
+				for j := 0; j < m; j++ {
+					d[j] += a[i][j]
+				}
+			}
+		}
+		ok := true
+		for i := 0; i < m; i++ {
+			if d[i] < x {
+				ok = false
+			}
+		}
+		if ok {
+			ans = min(ans, cost)
 		}
 	}
-	log.Print(s)
-
-	mi := 1 << 31
-	for j := 0; j < m; j++ {
-		mi = min(mi, s[n][j])
-	}
-	if mi < x {
+	// 答えが更新されていない場合は条件を満たす場合が無いので-1を返す
+	if ans == 1<<31 {
 		return -1
-	}
-
-	mi = 1 << 31
-	b, e := 0, 0
-	for i := n - 1; i >= 0; i-- {
-		for j := 0; j < m; j++ {
-			mi = min(mi, s[i][j])
-		}
-		if mi >= x {
-			e = i
-		}
-	}
-	log.Print(e)
-	for b = 0; b < n; b++ {
-		log.Print(s[b])
-		for j := 0; j < m; j++ {
-			mi = min(mi, s[e][j]-s[b][j])
-		}
-		if mi >= x {
-			break
-		}
-	}
-	log.Print(b)
-
-	ans := 0
-	for k := b; k <= e; k++ {
-		ans += c[k]
 	}
 	return ans
 }
