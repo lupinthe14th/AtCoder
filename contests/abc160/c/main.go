1920248a9db4129fa770ca38047cadf96fd52326
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-05-18 21:53:02 +0900

    abc160/C - Traveling Salesman around Lake: seen text

--- contests/abc160/c/main.go
+++ contests/abc160/c/main.go
@@ -1,59 +1,34 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
 )
 
 func solution(k, n int, nums []int) int {
-	var ans int = 1 << 31
-	var sum int
-	for i := 0; i < n; i++ {
-		for j := i; j < n-1; j++ {
-			if j+1 == n {
-				sum += k - nums[j] + nums[0]
-			} else {
-				sum += nums[j+1] - nums[j]
-			}
-		}
-		ans = min(ans, sum)
-	}
-	for i := 0; i < n; i++ {
-		for j := i; j < n-1; j-- {
-			if j == 0 {
-				sum += k - nums[j] - nums[n-1]
-			} else {
-				sum += nums[j] - nums[j-1]
-			}
-		}
-		ans = min(ans, sum)
-	}
-	return ans
-}
+	memo := make([]int, n)
+	memo[0] = nums[0] + k - nums[len(nums)-1]
 
-func min(x, y int) int {
-	if x < y {
-		return x
+	for i := 1; i < n; i++ {
+		memo[i] = nums[i] - nums[i-1]
 	}
-	return y
+	maxSoFar := memo[0]
+
+	for i := 1; i < n; i++ {
+		if memo[i] > maxSoFar {
+			maxSoFar = memo[i]
+		}
+	}
+	return k - maxSoFar
 }
 
 func main() {
-	sc := bufio.NewScanner(os.Stdin)
-	sc.Split(bufio.ScanWords)
-
-	sc.Scan()
-	k, _ := strconv.Atoi(sc.Text())
-
-	sc.Scan()
-	n, _ := strconv.Atoi(sc.Text())
-
-	nums := make([]int, n)
+	var k, n int
+	fmt.Scan(&k, &n)
+	nums := make([]int, 0, n)
 	for i := 0; i < n; i++ {
-		sc.Scan()
-		nums[i], _ = strconv.Atoi(sc.Text())
+		t := 0
+		fmt.Scan(&t)
+		nums = append(nums, t)
 	}
 
 	fmt.Println(solution(k, n, nums))
