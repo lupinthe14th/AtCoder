870c0ee0366787b9ac607fdd2290758d7c3574b3
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-05-20 18:09:42 +0900

    abc138/C - Alchemist: sort solution

---
+++ contests/abc138/c/main_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestSolution(t *testing.T) {
+	type in struct {
+		n int
+		v []int
+	}
+
+	var cases = []struct {
+		in   in
+		want float64
+	}{
+		{in: in{n: 2, v: []int{3, 4}}, want: 3.5},
+		{in: in{n: 3, v: []int{500, 300, 200}}, want: 375},
+	}
+	for i, tt := range cases {
+		t.Run(fmt.Sprint(i), func(t *testing.T) {
+			got := solution(tt.in.n, tt.in.v)
+			if got != tt.want {
+				t.Errorf("in: %+v, got: %v, want: %v", tt.in, got, tt.want)
+			}
+		})
+	}
+}
+
+func Example_main() {
+	c, _ := ioutil.ReadFile("./input.txt")
+	inr, inw, _ := os.Pipe()
+	orgStdin := os.Stdin
+	inw.Write(c)
+	inw.Close()
+	os.Stdin = orgStdin
+	defer func() { os.Stdin = orgStdin }()
+	os.Stdin = inr
+
+	main()
+
+	// Output: 138
+}
