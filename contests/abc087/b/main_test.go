249a554a019e136505d85a0c11dec8b92cdb633c
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-05-26 09:46:03 +0900

    abc087/B - Coins: bruteforce solution

---
+++ contests/abc087/b/main_test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestSolution(t *testing.T) {
+	type in struct {
+		a, b, c, x int
+	}
+
+	var cases = []struct {
+		in   in
+		want int
+	}{
+		{in: in{a: 2, b: 2, c: 2, x: 100}, want: 2},
+		{in: in{a: 5, b: 1, c: 0, x: 150}, want: 0},
+	}
+	for i, tt := range cases {
+		t.Run(fmt.Sprint(i), func(t *testing.T) {
+			got := solution(tt.in.a, tt.in.b, tt.in.c, tt.in.x)
+			if got != tt.want {
+				t.Errorf("in: %+v, got: %v, want: %v", tt.in, got, tt.want)
+			}
+		})
+	}
+}
+
+func Example_main() {
+	c, _ := ioutil.ReadFile("./input.txt")
+	inr, inw, _ := os.Pipe()
+	orgStdin := os.Stdin
+	inw.Write(c)
+	inw.Close()
+	os.Stdin = orgStdin
+	defer func() { os.Stdin = orgStdin }()
+	os.Stdin = inr
+
+	main()
+
+	// Output: 213
+}
