ee516f0a86586ca76e1d473581de94c21d9707cd
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-05-31 18:39:49 +0900

    abc141/B - Tap Dance: refactor code

---
+++ contests/abc141/b/main_test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestIsTapDanceEasy(t *testing.T) {
+	tests := []struct {
+		in   string
+		want string
+	}{
+		{in: "U", want: "Yes"},
+		{in: "RUDLUDR", want: "Yes"},
+		{in: "DULL", want: "No"},
+		{in: "UUUUUUUUUUUUUUU", want: "Yes"},
+		{in: "UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU", want: "Yes"},
+		{in: "ULURU", want: "No"},
+		{in: "RDULULDURURLRDULRLR", want: "Yes"},
+	}
+
+	for i, tt := range tests {
+		t.Run(fmt.Sprint(i), func(t *testing.T) {
+			got := solution(tt.in)
+			if got != tt.want {
+				t.Fatalf("in: %v got: %v want: %v", tt.in, got, tt.want)
+			}
+		})
+	}
+}
+
+func Example_main() {
+	fd, _ := os.Open("./input.txt")
+	orgStdin := os.Stdin
+	os.Stdin = fd
+	defer func() {
+		os.Stdin = orgStdin
+		fd.Close()
+	}()
+
+	main()
+
+	// Output: Yes
+}
