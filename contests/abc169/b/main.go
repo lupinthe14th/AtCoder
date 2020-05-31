4741dbab4a953409a01d236542b62ad0cc0478b2
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-05-31 23:34:31 +0900

    abc169/B - Multiplication 2: think overflow solution

---
+++ contests/abc169/b/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+func solution(n int, a []int) int {
+	var out int = 1
+
+	for _, num := range a {
+		if num == 0 {
+			return 0
+		}
+	}
+	for _, num := range a {
+		out *= num
+		if out > 1e18 || out < num {
+			return -1
+		}
+	}
+	return out
+}
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+	a := make([]int, 0, n)
+	for i := 0; i < n; i++ {
+		var t int
+		fmt.Scan(&t)
+		a = append(a, t)
+	}
+	fmt.Println(solution(n, a))
+}
