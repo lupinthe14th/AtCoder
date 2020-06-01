5b75d3190fef41b54aa01901c8b3c10e1b87efd1
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-06-02 00:16:53 +0900

    abc118/B - Foods Loved by Everyone: simple solution

---
+++ contests/abc118/b/main_test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestSolution(t *testing.T) {
+	type in struct {
+		n, m int
+		k    []int
+		a    [][]int
+	}
+	tests := []struct {
+		in   in
+		want int
+	}{
+		{in: in{n: 3, m: 4, k: []int{2, 3, 2}, a: [][]int{{1, 3}, {1, 2, 3}, {3, 2}}}, want: 1},
+		{in: in{n: 1, m: 30, k: []int{3}, a: [][]int{{5, 10, 30}}}, want: 3},
+	}
+	for i, tt := range tests {
+		t.Run(fmt.Sprint(i), func(t *testing.T) {
+			got := solution(tt.in.n, tt.in.m, tt.in.k, tt.in.a)
+			if got != tt.want {
+				t.Fatalf("in: %v got: %v want: %v", tt.in, got, tt.want)
+			}
+		})
+	}
+}
+
+func Example_main() {
+	fd, _ := os.Open("./input.txt")
+	orgStdin := os.Stdin
+	os.Stdin = fd
+	defer func() {
+		os.Stdin = orgStdin
+		fd.Close()
+	}()
+
+	main()
+
+	// Output: 0
+}
