2742d8793cd19a55fd522468ff62bd3d88734bc5
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-06-08 17:27:14 +0900

    abc083/B - Some Sums: simple solution

---
+++ contests/abc083/b/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+func solution(n, a, b int) int {
+	out := 0
+	for i := 1; i <= n; i++ {
+		t := sumDigit(i)
+		if t >= a && t <= b {
+			out += i
+		}
+	}
+	return out
+}
+
+func sumDigit(n int) int {
+	out := 0
+	for n > 0 {
+		out += n % 10
+		n /= 10
+	}
+	return out
+}
+
+func main() {
+	var n, a, b int
+	fmt.Scan(&n, &a, &b)
+	fmt.Println(solution(n, a, b))
+}
