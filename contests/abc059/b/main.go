912b43afd90518556ec62784fa52a23ae0c5f534
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-06-08 14:16:12 +0900

    abc059/B - Comparison: refactor code

--- contests/abc059/b/main.go
+++ contests/abc059/b/main.go
@@ -5,40 +5,19 @@ import (
 )
 
 func solution(a, b string) string {
-	l := max(len(a), len(b))
-	a = helper(a, l)
-	b = helper(b, l)
+	if len(a) > len(b) {
+		return "GREATER"
+	}
+	if len(a) < len(b) {
+		return "LESS"
+	}
 	if a > b {
 		return "GREATER"
 	}
-
 	if a == b {
 		return "EQUAL"
 	}
 	return "LESS"
-}
-
-func max(x, y int) int {
-	if x > y {
-		return x
-	}
-	return y
-}
-
-func helper(s string, l int) string {
-	sl := len(s)
-	if sl < l {
-		buf := make([]byte, 0, l)
-		for i := 0; i < l; i++ {
-			if i < l-sl {
-				buf = append(buf, '0')
-			} else {
-				buf = append(buf, s[i-l+sl])
-			}
-		}
-		return string(buf)
-	}
-	return s
 }
 
 func main() {
