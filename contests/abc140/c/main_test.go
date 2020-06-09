6da5782c92842a03ff3a8d6e6f795281cf4d3d7a
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-06-09 11:22:41 +0900

    abc140/C - Maximal Value: simple solution

---
+++ contests/abc140/c/main_test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestSolution(t *testing.T) {
+	type in struct {
+		n int
+		b []int
+	}
+	tests := []struct {
+		in   in
+		want int
+	}{
+		{in: in{n: 3, b: []int{2, 5}}, want: 9},
+		{in: in{n: 2, b: []int{3}}, want: 6},
+	}
+	for i, tt := range tests {
+		t.Run(fmt.Sprint(i), func(t *testing.T) {
+			got := solution(tt.in.n, tt.in.b)
+			if got != tt.want {
+				t.Fatalf("in: %v got: %v want: %v", tt.in, got, tt.want)
+			}
+		})
+	}
+}
+
+func Example_main() {
+	fd, _ := os.Open(filepath.Join("testdata", "input.txt"))
+	orgStdin := os.Stdin
+	os.Stdin = fd
+	defer func() {
+		os.Stdin = orgStdin
+		fd.Close()
+	}()
+
+	main()
+
+	// Output: 53
+}
