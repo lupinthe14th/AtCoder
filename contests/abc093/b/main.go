0b3a64a7be4ce581ff5c7816b5671d9c8c3d8b18
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-06-11 16:35:07 +0900

    abc093/B - Small and Large Integers: refactor code

--- contests/abc093/b/main.go
+++ contests/abc093/b/main.go
@@ -7,14 +7,29 @@ import (
 func solution(a, b, k int) []int {
 
 	out := make([]int, 0, 2*k)
-	for i := a; i <= b; i++ {
-		if i < a+k || b-k < i {
-			out = append(out, i)
-		}
+	for i := a; i <= min(b, a+k-1); i++ {
+		out = append(out, i)
+	}
+
+	for i := max(b-k+1, a+k); i <= b; i++ {
+		out = append(out, i)
 	}
 	return out
 }
 
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
 func main() {
 	var a, b, k int
 	fmt.Scan(&a, &b, &k)
