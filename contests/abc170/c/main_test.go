379075a676a70b0d8b0fea570200a2b047655826
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-06-15 14:47:15 +0900

    abc170/C - Forbidden List: sliding windows solution

---
+++ contests/abc170/c/main_test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestSolution(t *testing.T) {
+	type in struct {
+		x, n int
+		p    []int
+	}
+	tests := []struct {
+		in   in
+		want int
+	}{
+		{in: in{x: 6, n: 5, p: []int{4, 7, 10, 6, 5}}, want: 8},
+		{in: in{x: 100, n: 0, p: []int{}}, want: 100},
+	}
+	for i, tt := range tests {
+		t.Run(fmt.Sprint(i), func(t *testing.T) {
+			got := solution(tt.in.x, tt.in.n, tt.in.p)
+			if got != tt.want {
+				t.Fatalf("in: %v got: %v want: %v", tt.in, got, tt.want)
+			}
+		})
+	}
+}
+
+func Example_main() {
+	fd, _ := os.Open(filepath.Join("testdata", "input.txt"))
+	orgStdin := os.Stdin
+	os.Stdin = fd
+	defer func() {
+		os.Stdin = orgStdin
+		fd.Close()
+	}()
+
+	main()
+
+	// Output: 9
+}
