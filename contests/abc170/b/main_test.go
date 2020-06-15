6c6fadfa589dfd5e22f3b085dfb9b1af988bd845
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-06-15 14:46:03 +0900

    abc170/B - Crane and Turtle: simple solution

---
+++ contests/abc170/b/main_test.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestSolution(t *testing.T) {
+	type in struct {
+		x, y int
+	}
+	tests := []struct {
+		in   in
+		want string
+	}{
+		{in: in{x: 3, y: 8}, want: "Yes"},
+		{in: in{x: 1, y: 2}, want: "Yes"},
+	}
+	for i, tt := range tests {
+		t.Run(fmt.Sprint(i), func(t *testing.T) {
+			got := solution(tt.in.x, tt.in.y)
+			if got != tt.want {
+				t.Fatalf("in: %v got: %v want: %v", tt.in, got, tt.want)
+			}
+		})
+	}
+}
+
+func Example_main() {
+	fd, _ := os.Open(filepath.Join("testdata", "input.txt"))
+	orgStdin := os.Stdin
+	os.Stdin = fd
+	defer func() {
+		os.Stdin = orgStdin
+		fd.Close()
+	}()
+
+	main()
+
+	// Output: No
+}
