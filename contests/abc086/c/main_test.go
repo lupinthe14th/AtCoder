a1aa50255ea33bca67e546f908d0b6d2df67b9ca
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-06-17 11:07:58 +0900

    abc086/C - Traveling: physical solution

---
+++ contests/abc086/c/main_test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestSolution(t *testing.T) {
+	type in struct {
+		n      int
+		matrix [][3]int
+	}
+	tests := []struct {
+		in   in
+		want string
+	}{
+		{in: in{n: 2, matrix: [][3]int{{3, 1, 2}, {6, 1, 1}}}, want: "Yes"},
+		{in: in{n: 1, matrix: [][3]int{{2, 100, 100}}}, want: "No"},
+	}
+
+	for i, tt := range tests {
+		t.Run(fmt.Sprint(i), func(t *testing.T) {
+			got := solution(tt.in.n, tt.in.matrix)
+			if got != tt.want {
+				t.Fatalf("in: %v got: %v want: %v", tt.in, got, tt.want)
+			}
+		})
+	}
+}
+
+func Example_main() {
+	fd, _ := os.Open(filepath.Join("testdata", "input.txt"))
+	orgStdin := os.Stdin
+	os.Stdin = fd
+	defer func() {
+		os.Stdin = orgStdin
+		fd.Close()
+	}()
+
+	main()
+
+	// Output: No
+}
