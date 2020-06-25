0d0ae76c5fa5609f2b06ed153c720d47eab2a4c5
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-06-25 14:57:27 +0900

    abc100/C - *3 or /2: seen refarence

---
+++ contests/abc100/c/main_test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestSolution(t *testing.T) {
+	type in struct {
+		n int
+		a []int
+	}
+	tests := []struct {
+		in   in
+		want int
+	}{
+		{in: in{n: 3, a: []int{5, 2, 4}}, want: 3},
+		{in: in{n: 4, a: []int{631, 577, 243, 199}}, want: 0},
+	}
+	for i, tt := range tests {
+		t.Run(fmt.Sprint(i), func(t *testing.T) {
+			got := solution(tt.in.n, tt.in.a)
+			if got != tt.want {
+				t.Fatalf("in: %v got: %v want: %v", tt.in, got, tt.want)
+			}
+		})
+	}
+}
+
+func Example_main() {
+	fd, _ := os.Open(filepath.Join("testdata", "input.txt"))
+	orgStdin := os.Stdin
+	os.Stdin = fd
+	defer func() {
+		os.Stdin = orgStdin
+		fd.Close()
+	}()
+
+	main()
+
+	// Output: 39
+}
