79131a24082fa9654785fd68ce4787cf239fe9ba
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-07-03 09:24:56 +0900

    abc103/C - Modulo Summation: math solution

---
+++ contests/abc103/c/main_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestSolution(t *testing.T) {
+	type in struct {
+		n int
+		a []int
+	}
+	tests := []struct {
+		in   in
+		want int
+	}{
+		{in: in{n: 3, a: []int{3, 4, 6}}, want: 10},
+		{in: in{n: 5, a: []int{7, 46, 11, 20, 11}}, want: 90},
+	}
+	for i, tt := range tests {
+		i, tt := i, tt
+		t.Run(fmt.Sprint(i), func(t *testing.T) {
+			t.Parallel()
+			got := solution(tt.in.n, tt.in.a)
+			if got != tt.want {
+				t.Fatalf("in: %v got: %v want: %v", tt.in, got, tt.want)
+			}
+		})
+	}
+}
+
+func Example_main() {
+	fd, _ := os.Open(filepath.Join("testdata", "input.txt"))
+	orgStdin := os.Stdin
+	os.Stdin = fd
+	defer func() {
+		os.Stdin = orgStdin
+		fd.Close()
+	}()
+
+	main()
+
+	// Output: 4527
+}
