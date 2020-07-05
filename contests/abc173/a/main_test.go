41e70f67b347b474182e6a93ec88e466185cee6b
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-07-05 23:02:16 +0900

    abc173/A - Payment: simple solution

---
+++ contests/abc173/a/main_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"os"
+	"path/filepath"
+)
+
+func Example_main() {
+	fd, _ := os.Open(filepath.Join("testdata", "input.txt"))
+	orgStdin := os.Stdin
+	os.Stdin = fd
+	defer func() {
+		os.Stdin = orgStdin
+		fd.Close()
+	}()
+
+	main()
+
+	// Output: 100
+}
