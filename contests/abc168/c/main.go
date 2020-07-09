c3e94cff3ac6079db866ae5338c331c04aeea68c
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-07-09 21:56:01 +0900

    abc168/C - : (Colon): refactor code

--- contests/abc168/c/main.go
+++ contests/abc168/c/main.go
@@ -2,21 +2,16 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"math"
 )
 
 func solution(a, b, h, m float64) float64 {
 	M := 6 * m
 	H := (h / 12 * 360) + (m / 60 * 30)
-	log.Printf("H: %v M: %v", H, M)
 	A := abs(H, M)
 	if A >= 180 {
 		A = 360 - A
 	}
-	log.Print(A)
-	log.Print(math.Pow(a, 2))
-	log.Print(math.Pow(b, 2))
 	return math.Sqrt(math.Pow(a, 2) + math.Pow(b, 2) - 2*a*b*math.Cos(A*(math.Pi/180)))
 }
 
