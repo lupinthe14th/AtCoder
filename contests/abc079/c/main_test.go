826b0dd904e971c92793bf06caddcc0e51c952dc
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-07-13 16:41:12 +0900

    abc079/C - Train Ticket: using bit全探索

---
+++ contests/abc079/c/main_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestSolution(t *testing.T) {
+	tests := []struct {
+		in, want string
+	}{
+		{in: "1222", want: "1+2+2+2=7"},
+		{in: "0290", want: "0-2+9+0=7"},
+	}
+	for i, tt := range tests {
+		t.Run(fmt.Sprint(i), func(t *testing.T) {
+			got := solution(tt.in)
+			if got != tt.want {
+				t.Fatalf("in: %v got: %v want: %v", tt.in, got, tt.want)
+			}
+		})
+	}
+}
+
+func Example_main() {
+	fd, _ := os.Open(filepath.Join("testdata", "input.txt"))
+	orgStdin := os.Stdin
+	os.Stdin = fd
+	defer func() {
+		os.Stdin = orgStdin
+		fd.Close()
+	}()
+
+	main()
+
+	// Output: 3+2+4-2=7
+}
