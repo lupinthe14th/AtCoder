df6de594c8768ff2159d38b825c36bff2dd29054
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-07-28 13:22:03 +0900

    abc106/B - 105: simple solution

---
+++ contests/abc106/b/main_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestSolution(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		in, want int
+	}{
+		{in: 105, want: 1},
+		{in: 7, want: 0},
+	}
+	for i, tt := range tests {
+		i, tt := i, tt
+		t.Run(fmt.Sprint(i), func(t *testing.T) {
+			t.Parallel()
+			got := solution(tt.in)
+			if got != tt.want {
+				t.Fatalf("in: %v got: %v want: %v", tt.in, got, tt.want)
+			}
+		})
+	}
+}
+
+func Example_main() {
+	fd, _ := os.Open(filepath.Join("testdata", "input.txt"))
+	orgStdin := os.Stdin
+	os.Stdin = fd
+	defer func() {
+		os.Stdin = orgStdin
+		fd.Close()
+	}()
+
+	main()
+
+	// Output: 0
+}
