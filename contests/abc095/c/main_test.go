3b790b80d4097f81d396e5f7cf9a45267971b9b1
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-07-29 17:20:58 +0900

    abc095/C - Half and Half: 0(1) solution

--- contests/abc095/c/main_test.go
+++ contests/abc095/c/main_test.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
+	"path/filepath"
 	"testing"
 )
 
@@ -13,34 +13,33 @@ func TestSolution(t *testing.T) {
 		a, b, c, x, y int
 	}
 
-	var cases = []struct {
+	var tests = []struct {
 		in   in
 		want int
 	}{
 		{in: in{a: 1500, b: 2000, c: 1600, x: 3, y: 2}, want: 7900},
 		{in: in{a: 1500, b: 2000, c: 1900, x: 3, y: 2}, want: 8500},
 	}
-	for i, tt := range cases {
+	for i, tt := range tests {
 		i, tt := i, tt
 		t.Run(fmt.Sprint(i), func(t *testing.T) {
 			t.Parallel()
 			got := solution(tt.in.a, tt.in.b, tt.in.c, tt.in.x, tt.in.y)
 			if got != tt.want {
-				t.Errorf("in: %+v, got: %v, want: %v", tt.in, got, tt.want)
+				t.Fatalf("in: %+v, got: %v, want: %v", tt.in, got, tt.want)
 			}
 		})
 	}
 }
 
 func Example_main() {
-	c, _ := ioutil.ReadFile("./input.txt")
-	inr, inw, _ := os.Pipe()
+	fd, _ := os.Open(filepath.Join("testdata", "input.txt"))
 	orgStdin := os.Stdin
-	inw.Write(c)
-	inw.Close()
-	os.Stdin = orgStdin
-	defer func() { os.Stdin = orgStdin }()
-	os.Stdin = inr
+	os.Stdin = fd
+	defer func() {
+		os.Stdin = orgStdin
+		fd.Close()
+	}()
 
 	main()
 
