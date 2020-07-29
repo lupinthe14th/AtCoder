57826ccc040d41ed3f0afd649e89930182507e69
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-07-29 15:54:18 +0900

    abc122/B - ATCoder: simple solution

--- contests/abc122/b/main.go
+++ contests/abc122/b/main.go
@@ -5,6 +5,24 @@ import (
 )
 
 func solution(s string) int {
+	max := func(x, y int) int {
+		if x > y {
+			return x
+		}
+		return y
+	}
+
+	isACGT := func(s string) bool {
+		for _, r := range s {
+			if r == 'A' || r == 'C' || r == 'G' || r == 'T' {
+				continue
+			} else {
+				return false
+			}
+		}
+		return true
+	}
+
 	l := len(s)
 	out := 0
 	for i := 0; i <= l; i++ {
@@ -16,24 +34,6 @@ func solution(s string) int {
 		}
 	}
 	return out
-}
-
-func max(x, y int) int {
-	if x > y {
-		return x
-	}
-	return y
-}
-
-func isACGT(s string) bool {
-	for _, r := range s {
-		if r == 'A' || r == 'C' || r == 'G' || r == 'T' {
-			continue
-		} else {
-			return false
-		}
-	}
-	return true
 }
 
 func main() {
