bb03f47c4a17c8deba9a39e536d4525113ea06cb
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-07-30 14:06:20 +0900

    sumitrust2019/D - Lucky PIN: seen editorial. dp approach

--- contests/sumitrust2019/d/main.go
+++ contests/sumitrust2019/d/main.go
@@ -4,22 +4,29 @@ import (
 	"fmt"
 )
 
-// seen: https://atcoder.jp/contests/sumitrust2019/submissions/8695967
+// seen: https://atcoder.jp/contests/sumitrust2019/submissions/8717655
 func solution(n int, s string) int {
-	out := 0
+	var dp [30009][4][1009]bool
+	dp[0][0][0] = true
 
-	for i := 0; i < 1000; i++ {
-		b := [3]byte{byte(i / 100), byte((i / 10) % 10), byte(i % 10)}
-		f := 0
-		for j := 0; j < n; j++ {
-			if s[j] == ('0' + b[f]) {
-				f++
-			}
-			if f == 3 {
-				break
+	for i := 0; i < n; i++ {
+		for j := 0; j <= 3; j++ {
+			for k := 0; k < 1000; k++ {
+				if !dp[i][j][k] {
+					continue
+				}
+
+				dp[i+1][j][k] = true
+				if j <= 2 {
+					dp[i+1][j+1][k*10+int(s[i]-'0')] = true
+				}
 			}
 		}
-		if f == 3 {
+	}
+
+	out := 0
+	for i := 0; i < 1000; i++ {
+		if dp[n][3][i] {
 			out++
 		}
 	}
