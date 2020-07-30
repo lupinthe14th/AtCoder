222bc6bf80001139eda3aa12e37a1a45ac531d39
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-07-30 13:50:45 +0900

    sumitrust2019/D - Lucky PIN: seen editorial. greedy approach

---
+++ contests/sumitrust2019/d/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+func solution(n int, s string) int {
+	out := 0
+
+	for i := 0; i < 1000; i++ {
+		b := [3]byte{byte(i / 100), byte((i / 10) % 10), byte(i % 10)}
+		f := 0
+		for j := 0; j < n; j++ {
+			if s[j] == ('0' + b[f]) {
+				f++
+			}
+			if f == 3 {
+				break
+			}
+		}
+		if f == 3 {
+			out++
+		}
+	}
+	return out
+}
+
+func main() {
+	var n int
+	var s string
+	fmt.Scan(&n, &s)
+	fmt.Println(solution(n, s))
+}
