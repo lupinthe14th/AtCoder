132203938cb38cf6f4ffadfdeeb7b89d77028111
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-08-03 15:22:33 +0900

    第７回日本情報オリンピック 予選（オンライン）/D - 星座探し: simple solution

---
+++ contests/joi2008yo/d/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+)
+
+func solution(m, n int, p, q [][2]int) [2]int {
+	memo := make(map[[2]int]bool)
+	for i := range q {
+		memo[q[i]] = true
+	}
+
+	p0x := p[0][0]
+	p0y := p[0][1]
+	var out [2]int
+	for i := range q {
+		dx := q[i][0] - p0x
+		dy := q[i][1] - p0y
+		out = [2]int{dx, dy}
+		flag := false
+		for j := range p {
+			x := p[j][0] + dx
+			y := p[j][1] + dy
+			if !memo[[2]int{x, y}] {
+				flag = false
+				break
+			}
+			flag = true
+		}
+		if flag {
+			break
+		}
+	}
+	return out
+}
+
+func main() {
+	var m int
+	fmt.Scan(&m)
+	p := make([][2]int, m)
+	for i := range p {
+		fmt.Scan(&p[i][0], &p[i][1])
+	}
+	var n int
+	fmt.Scan(&n)
+	q := make([][2]int, n)
+	for i := range q {
+		fmt.Scan(&q[i][0], &q[i][1])
+	}
+	ans := solution(m, n, p, q)
+	fmt.Printf("%v %v\n", ans[0], ans[1])
+}
